1647d1809db5482c34e4a3337b5fc05b031fae6d
yang sun <yangsun@yangs-MacBook-Pro.local>
2020-07-28 01:32:57 -0700

    finish up

--- static/js/app.js
+++ static/js/app.js
@@ -26,6 +26,8 @@ d3.json("../samples.json").then((importdata)=>{
         // 1.create default xy, layout and plot
         // 2.only plot first 10 otu value in a descending order
         // 3.
+        var config = {responsive:true};
+
         var otuId = [];
         for (var i=9;i>=0;i--){
             otuId.push("OTU ".concat(data.samples[0].otu_ids[i]));
@@ -38,7 +40,7 @@ d3.json("../samples.json").then((importdata)=>{
         for (var i=9;i>=0;i--){
             hoverText.push(data.samples[0].otu_labels[i]);
         };
-        console.log(data.samples[0])
+        console.log(hoverText);
         var trace = {
             x:otuValue,
             y:otuId,
@@ -49,7 +51,7 @@ d3.json("../samples.json").then((importdata)=>{
         var layout = {
             title:`Subject: ${testSubjectId[0]}`
         };
-        Plotly.newPlot("bar",[trace],layout);// barchart
+        Plotly.newPlot("bar",[trace],layout,config);// barchart
 
         /*************************************** Bubble chart *********************************************/
         /*================================================================================================*/
@@ -71,7 +73,8 @@ d3.json("../samples.json").then((importdata)=>{
                 }
             },
         };
-        Plotly.newPlot("bubble", [traceBubble], layoutBubble);
+        
+        Plotly.newPlot("bubble", [traceBubble], layoutBubble,config);
 
         /************************************ Demographic Info *******************************************/
         /*===============================================================================================*/
@@ -80,8 +83,101 @@ d3.json("../samples.json").then((importdata)=>{
         for (var i=0;i<demoKeys.length;i++){
             demoInfo.append("h6").text(`${demoKeys[i]}: ${demoValues[i]}`)
         };
+
+        /***************************************** Gauge Chart *******************************************/
+        /*===============================================================================================*/
+        var firstWFreq = data.metadata[0].wfreq
+        // Calculations for gauge needle
+        var firstWFreqDeg = firstWFreq * 20;
+        var degrees = 180 - firstWFreqDeg;
+        var radius = 0.5;
+        var radians = (degrees * Math.PI) / 180;
+        var x = radius * Math.cos(degrees * Math.PI / 180);
+        var y = radius * Math.sin(degrees * Math.PI / 180);
+
+        // create path for gauge needle
+        // Create path for gauge needle
+        var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+        var mainPath = path1,
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+        // create trace for gauge chart
+        var dataGauge = [
+            {
+              type: "scatter",
+              x: [0],
+              y: [0],
+              marker: { size: 12, color: "850000" },
+              showlegend: false,
+              name: "Freq",
+              text: firstWFreq,
+              hoverinfo: "text+name"
+            },
+            {
+              values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+              rotation: 90,
+              text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+              textinfo: "text",
+              textposition: "inside",
+              marker: {
+                colors: [
+                  "rgba(0, 105, 11, .5)",
+                  "rgba(10, 120, 22, .5)",
+                  "rgba(14, 127, 0, .5)",
+                  "rgba(110, 154, 22, .5)",
+                  "rgba(170, 202, 42, .5)",
+                  "rgba(202, 209, 95, .5)",
+                  "rgba(210, 206, 145, .5)",
+                  "rgba(232, 226, 202, .5)",
+                  "rgba(240, 230, 215, .5)",
+                  "rgba(255, 255, 255, 0)"
+                ]
+              },
+              labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+              hoverinfo: "label",
+              hole: 0.5,
+              type: "pie",
+              showlegend: false
+            }
+          ];
+        
+        
+        console.log(dataGauge[0]);
+        // Create the layout for the gauge chart
+        var layoutGauge = {
+            shapes: [
+              {
+                type: "path",
+                path: path,
+                fillcolor: "850000",
+                line: {
+                  color: "850000"
+                }
+              }
+            ],
+            title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+            // height: 500,
+            // width: 500,
+            xaxis: {
+              zeroline: false,
+              showticklabels: false,
+              showgrid: false,
+              range: [-1, 1]
+            },
+            yaxis: {
+              zeroline: false,
+              showticklabels: false,
+              showgrid: false,
+              range: [-1, 1]
+            }
+          };
+          Plotly.newPlot('gauge', dataGauge,layoutGauge,config);
     };
-    
+
 
 
 
@@ -133,21 +229,70 @@ d3.json("../samples.json").then((importdata)=>{
         var otuIdBubble = data.samples[indexNumber].otu_ids;
         var otuValueBubble = data.samples[indexNumber].sample_values;
 
+        // update gauge value
+        var gaugeValue = data.metadata[indexNumber].wfreq
+        console.log(gaugeValue);
+
+        // update each plot and demographic info
         updatePlotly("bar",otuValue,otuId,hoverText,layout);
         updatePlotly("bubble",otuValueBubble,otuIdBubble,hoverText,layout);
         updateDemoInfo(data.metadata[indexNumber]);
+        updateGauge("gauge",gaugeValue);
     };
 
     function updatePlotly(plotType,otuValue,otuId,hoverText,layout){
+    
         Plotly.restyle(plotType, "x", [otuValue]);
         Plotly.restyle(plotType, "y", [otuId]);
         Plotly.restyle(plotType, "text", [hoverText]);
         if (plotType="bar"){
             Plotly.relayout(plotType,layout);
         };
-        
-    }
+    };
+     
+    function updateGauge(plotType,firstWFreq){
+        var dataGauge = [
+            {
+              type: "scatter",
+              x: [0],
+              y: [0],
+              marker: { size: 12, color: "850000" },
+              showlegend: false,
+              name: "Freq",
+              text: firstWFreq,
+              hoverinfo: "text+name"
+            },
+            {
+              values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+              rotation: 90,
+              text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+              textinfo: "text",
+              textposition: "inside",
+              marker: {
+                colors: [
+                  "rgba(0, 105, 11, .5)",
+                  "rgba(10, 120, 22, .5)",
+                  "rgba(14, 127, 0, .5)",
+                  "rgba(110, 154, 22, .5)",
+                  "rgba(170, 202, 42, .5)",
+                  "rgba(202, 209, 95, .5)",
+                  "rgba(210, 206, 145, .5)",
+                  "rgba(232, 226, 202, .5)",
+                  "rgba(240, 230, 215, .5)",
+                  "rgba(255, 255, 255, 0)"
+                ]
+              },
+              labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+              hoverinfo: "label",
+              hole: 0.5,
+              type: "pie",
+              showlegend: false
+            }
+          ];
 
+        Plotly.restyle(plotType,dataGauge);
+    };
+   
     function updateDemoInfo(metadata){
         // clear up existing content
         d3.selectAll("#sample-metadata").html("");
